849063d800c62581da3189466f632bbef07f070e
Marco Blum <marco8jnv@hotmail.com>
2018-02-11 07:19:48 +0100

    web ui done

--- main.js
+++ main.js
@@ -1,4 +1,46 @@
 var MongoClient = require('mongodb').MongoClient;
+let express = require('express');
+let app = express();
+console.log(__dirname+'/public/index.html')
+app.get('/',(req,res)=>{
+    
+    res.sendFile(__dirname+'/public/index.html');
+})
+app.get('/users',(req,res)=>{
+    func_number_of_users((item)=>{
+        res.send(JSON.stringify(item));
+    })
+})
+app.get('/linkers/:x',(req,res)=>{
+    func_number_of_links(parseInt(req.param('x')),(item)=>{
+        res.send(JSON.stringify(item));
+    })
+})
+app.get('/active/:x',(req,res)=>{
+    func_number_of_posts(parseInt(req.param('x')),(item)=>{
+        res.send(JSON.stringify(item));
+    })
+})
+app.get('/mentioned/:x',(req,res)=>{
+    func_most_mentioned(parseInt(req.param('x')),(item)=>{
+        res.send(JSON.stringify(item));
+    })
+})
+app.get('/words/:x/:adj/:words',(req,res)=>{
+    console.log("received words: ",req.param('words'))
+    func_most_words(parseInt(req.param('x')),req.param('adj'),req.param('words').split(','),(item)=>{
+        res.send(JSON.stringify(item));
+    })
+})
+
+let server = app.listen(3333, ()=>{
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log('server running at http://%s:%s',host,port)
+})
+
+
+
 let db_name = undefined;
 let col_name = undefined;
 if (process.argv.length != 4) {
@@ -9,7 +51,7 @@ else {
     col_name = process.argv[3];
     console.log("\n------------------running code initiating coffee break--------------------\n")
 }
-function func_number_of_users() {
+function func_number_of_users(callback) {
     let start_time = new Date();
     MongoClient.connect("mongodb://localhost:27017/"+db_name, function (err, database) {
         if (err) { return console.dir(err); }
@@ -17,11 +59,13 @@ function func_number_of_users() {
         collection.distinct('user').then(function (item) {
             console.log("Total time spent:"+(new Date()-start_time)+"ms","number of users: ", item.length);
             console.log("\n--------------------------------------\n")
+            
             database.close()
+            callback(item.length)
         })
     });
 }
-function func_number_of_links(top_x) {
+function func_number_of_links(top_x,callback) {
     let start_time = new Date();
     MongoClient.connect("mongodb://localhost:27017/"+db_name, function (err, database) {
         if (err) { return console.dir(err); }
@@ -39,10 +83,11 @@ function func_number_of_links(top_x) {
                 }
                 console.log("\n--------------------------------------\n")
                 database.close();
+                callback(item)
             });
     })
 }
-function func_number_of_posts(top_x) {
+function func_number_of_posts(top_x,callback) {
     let start_time = new Date();
     MongoClient.connect("mongodb://localhost:27017/"+db_name, function (err, database) {
         if (err) { return console.dir(err); }
@@ -60,15 +105,17 @@ function func_number_of_posts(top_x) {
                 }
                 console.log("\n--------------------------------------\n")
                 database.close();
+                callback(item)
             });
     })
 }
-function func_most_words(top_x, adj, words) {
+function func_most_words(top_x, adj, words,callback) {
     let start_time = new Date();
     let obj = [];
     words.forEach((ele) => {
         obj.push({words:{$regex:ele+".",$options:'ism'}})
     });
+    console.log(obj)
     MongoClient.connect("mongodb://localhost:27017/"+db_name, function (err, database) {
         if (err) { return console.dir(err); }
         let collection = database.db(db_name).collection(col_name)
@@ -88,10 +135,11 @@ function func_most_words(top_x, adj, words) {
                     console.log("\n--------------------------------------\n")
                 }
                 database.close()
+                callback(item)
             });
     });
 }
-function func_most_mentioned(top_x){
+function func_most_mentioned(top_x,callback){
     let start_time = new Date();
     MongoClient.connect("mongodb://localhost:27017/"+db_name, function (err, database) {
         if (err) { return console.dir(err); }
@@ -112,9 +160,11 @@ function func_most_mentioned(top_x){
                     console.log("\n--------------------------------------\n")
                 }
                 database.close()
+                callback(item)
             });
     });
 }
+/*
 if(db_name != undefined && col_name != undefined){
     func_number_of_users();
     func_number_of_links(10);
@@ -122,4 +172,4 @@ if(db_name != undefined && col_name != undefined){
     func_most_mentioned(10)
     func_most_words(5,"bad",["fuck","fat","feminist","shit","asshole","cunt","kill","whore","bieber"])
     func_most_words(5,"good",["good","nice","sweet","love","awesome","blessed","beauty","holy"])
-}
+}*/
